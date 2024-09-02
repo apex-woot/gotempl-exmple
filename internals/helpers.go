02fe2180a6de2c6ac2bfe1e0294329302138bba7
Vitalii Kohut <vkohut@intelliarts.com>
2024-09-02 12:59:27 +0300

    fix: env

--- internals/helpers.go
+++ internals/helpers.go
@@ -2,14 +2,12 @@ package internals
 
 import (
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
 func GetEnvVariable(key string) string {
-	_ = godotenv.Load()
-	// if err != nil {
-	// 	fmt.Println("Error loading .env file, assuming this is PROD")
+	// env := godotenv.Load()
+	// if env != nil {
+	// 	fmt.Println("cannot find environment variables from file")
 	// }
 	return os.Getenv(key)
 }
